7ce03dc21463305ab362efd520bbf24091ddc716
salbeoo <82999092+salbeoo@users.noreply.github.com>
2022-03-11 13:08:31 +0100

    Create JFrom.java

---
+++ TelegramApi/src/TelegramApi/JFrom.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TelegramApi;
+
+/**
+ *
+ * @author stagno_alberto
+ */
+public class JFrom {
+    
+}
